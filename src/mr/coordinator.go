456bd37087a362caf50c6258c7e7df934e90e21f
LinkWoong <zuiaineo@foxmail.com>
2022-07-27 23:44:14 -0700

    Add gRPC calls and comm model definition

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,19 +1,39 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"io/ioutil"
+)
 
 
 type Coordinator struct {
 	// Your definitions here.
-
+	Files []string
+	NReduce int
+	Completed []bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
-
+func (c *Coordinator) TaskDistribution(args *MapWorkerTaskArgs, reply *MapWorkerTaskReply) error {
+	filename := c.Files[0]
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	args.Filename = filename
+	args.Content = string(content)
+	c.Files = c.Files[1:] // pop the first element as it is a queue
+	return nil
+}
 //
 // an example RPC handler.
 //
@@ -61,9 +81,13 @@ func (c *Coordinator) Done() bool {
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
+	// fmt.Println(len(files)) -> 8
+	// fmt.Println(files[0]) -> ./data/pg-being_ernest.txt
 	// Your code here.
 
+	c.Files = files
+	c.NReduce = nReduce
+	c.Completed = make([]bool, nReduce)
 
 	c.server()
 	return &c
